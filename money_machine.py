b6f54309965a4d788f2095e33d0b876721743fa0
jajakanjas <kejormie@gmail.com>
2025-03-15 03:15:31 +0700

    Memperbarui script make_payment

--- money_machine.py
+++ money_machine.py
@@ -25,3 +25,16 @@ class MoneyMachine:
         return self.money_received
 
     
+def make_payment(self, cost):
+    """Returns True when payment is accepted, or False if insufficient."""
+    self.process_coins()
+    if self.money_received >= cost:
+        change = round(self.money_received - cost, 2)
+        print(f"Here is {self.CURRENCY}{change} in change.")
+        self.profit += cost
+        self.money_received = 0
+        return True
+    else:
+        print("Sorry that's not enough money. Money refunded.")
+        self.money_received = 0
+        return False
